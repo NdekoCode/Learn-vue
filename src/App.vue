0857220998ad42e40b6586625a728e8f35f752e0
NdekoCode <arickbulakali@gmail.com>
2023-01-31 04:45:00 +0200

    init: animation with vuejs

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,20 @@
       <Counter />
       <Counter />
       <Counter />
-      <Counter />
+      <Alert />
     </div>
   </div>
 </template>
 
 <script>
+import Alert from "./components/Alert";
 import Counter from "./components/Counter.vue";
 export default {
   name: "App",
   components: {
     // HelloWorld,
     Counter,
+    Alert,
   },
 };
 </script>
---
+++ src/components/Alert.vue
@@ -0,0 +1,50 @@
+<template>
+  <transition name="fade">
+    <div
+      class="alert alert-success alert-dismissible fade show"
+      role="alert"
+      v-if="visible"
+    >
+      <button
+        type="button"
+        class="btn-close"
+        data-bs-dismiss="alert"
+        aria-label="Close"
+        @click="toggle"
+      ></button>
+      <strong>Alert Heading</strong> {{ message }}
+    </div>
+  </transition>
+</template>
+<script>
+export default {
+  name: "Alert",
+  data() {
+    return {
+      visible: true,
+    };
+  },
+  methods: {
+    toggle() {
+      this.visible = !this.visible;
+    },
+  },
+  props: {
+    message: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  opacity: 0.3s;
+}
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
+  transform: translateX(20px);
+}
+</style>
