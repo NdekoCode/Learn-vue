8f4cf2cd3f7edf35176394d0b410658a3650cb2f
NdekoCode <arickbulakali@gmail.com>
2023-07-23 20:43:19 +0200

    feat: add transition on route navigation

--- src/layouts/UserLayout.vue
+++ src/layouts/UserLayout.vue
@@ -1,6 +1,27 @@
 <template>
   <div class="p-3 mt-5 bg-red-100">
-    <router-view />
+    <router-view v-slot="{ Component }">
+      <transition name="fade" mode="out-in" appear>
+        <component :is="Component" />
+      </transition>
+    </router-view>
   </div>
 </template>
 <script setup></script>
+<style scoped>
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+  transform: scale(0.7) translateY(-15px);
+}
+.fade-enter-to,
+.fade-leave-from {
+  opacity: 1;
+  transform: scale(1) translateY(0);
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.35s, transform 0.35s;
+}
+</style>
