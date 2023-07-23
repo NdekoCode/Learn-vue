1c37a0ff06644ea82a5b8be4c25ae15095d8cd0c
NdekoCode <arickbulakali@gmail.com>
2023-07-23 13:01:10 +0200

    refactor: router params with props

--- src/pages/ViewUser.vue
+++ src/pages/ViewUser.vue
@@ -1,25 +1,30 @@
 <template>
-  User id {{ id }}
-  <div class="shadow-xl card card-side bg-base-100">
-    <div class="card-body">
-      <h2 class="card-title">{{ user.name }}</h2>
-      <p>{{ user.email }}</p>
-      <div class="justify-end card-actions">
-        <button class="btn btn-primary">Watch</button>
+  <div class="container px-4 mx-auto max-w-7xl lg:px-0">
+    <div class="border border-gray-100 shadow-xl card card-side bg-base-100">
+      <div class="card-body">
+        <h2 class="card-title">{{ user.name }}</h2>
+        <p>{{ user.email }}</p>
+        <p>User id {{ id }}</p>
+        <div class="justify-end card-actions">
+          <button class="btn btn-primary">Watch</button>
+        </div>
       </div>
     </div>
   </div>
 </template>
 <script setup>
 import { onMounted, ref } from "vue";
-import { useRoute } from "vue-router";
-const route = useRoute();
-const id = ref(123);
+const { id } = defineProps({
+  id: {
+    type: Number,
+  },
+});
+const userId = ref(id || null);
 const user = ref({});
 onMounted(async () => {
-  id.value = route.params.id;
+  userId.value = id;
   const res = await fetch(
-    "https://jsonplaceholder.typicode.com/users/" + id.value
+    "https://jsonplaceholder.typicode.com/users/" + userId.value
   );
   const resData = await res.json();
   if (res.status === 200) {
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,3 +1,4 @@
+import page404 from "@/pages/page404.vue";
 import Blog from "../pages/Blog.vue";
 import Home from "../pages/Home.vue";
 import Users from "../pages/Users.vue";
@@ -13,14 +14,20 @@ const routes = [
     component: Users,
   },
   {
-    path: "/users/:id",
+    path: "/users/:id(\\d+)",
     name: "users.show",
+    props: true,
     component: () => import("@/pages/ViewUser.vue"),
   },
   {
     path: "/blog",
     name: "blog",
     component: Blog,
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "404",
+    component: page404,
   },
 ];
 
